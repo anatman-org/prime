80849e10010c7fa2f7b64720fe7db6180a0f0c5f
Thornton Prime <thornton.prime@gmail.com>
2023-11-08 15:16:49 -0800

    updated config/tasks

--- config.py
+++ config.py
@@ -11,41 +11,39 @@ def username():
     return pwd.getpwuid(os.getuid())[0]
 
 def ai():
-    return ""
+    return "d62b31da"
 
-AI=ai
+
+AI=ai()
 USER=username()
+SEQUENCE=
 
 ###############################################################################
 DEBUG = False
 
-MEDIA_BASE_DIR = "/data"
+MEDIA_BASE_DIR = "/work"
 # MEDIA_BASE_DIR = str(Path().home())
 # MEDIA_BASE_DIR = str(Path().cwd())
 
-OUTPUT_BASE = MEDIA_BASE_DIR + "/play"
-# FNAME_TEMPLATE = OUTPUT_BASE + "/{config.AI()}/{now:%Y%m%d-%H%M%S-%f}.png"
-FNAME_TEMPLATE = OUTPUT_BASE + "/{config.USER}/{now:%Y%m%d-%H%M%S-%f}.png"
-# FNAME_TEMPLATE = OUTPUT_BASE + "/{now:%Y%m%d}/{now:%H%M%S-%f}.png"
+FNAME_TEMPLATE = MEDIA_BASE_DIR + "/play/{SEQUENCE}/{now:%Y%m%d-%H%M%S-%f}.png"
 
-
+# FNAME_TEMPLATE = MEDIA_BASE_DIR + "/out/{config.AI}/{now:%Y%m%d-%H%M%S-%f}.png"
+# FNAME_TEMPLATE = MEDIA_BASE_DIR + "/out/{config.USER}/{now:%Y%m%d-%H%M%S-%f}.png"
+# FNAME_TEMPLATE = MEDIA_BASE_DIR + "/out/{now:%Y%m%d}/{now:%H%M%S-%f}.png"
 
 ###############################################################################
 
+# BACK = MatSpin(file=MEDIA_BASE_DIR + "/img/enso.png", rotation_speed=6)
 # BACK = MatImage(MEDIA_BASE_DIR + "/background.png")
+BACK = MatVideo(MEDIA_BASE_DIR + "/cuts/ttb640a07bbbfdac_1.mp4", volume=0, loop=True)
 
 # _BACK_FILES = [str(f) for f in Path(MEDIA_BASE_DIR).glob("out/*/*.png")]
 # shuffle(_BACK_FILES)
 # BACK = MatImageList(_BACK_FILES)
 
-# BACK = MatVideo(MEDIA_BASE_DIR + "/clips/A Time for Renewal - Navajo (Diné) Knowledge of Eclipses [6332586887112].mp4", volume=0, loop=True)
-BACK = MatSpin(file="img/enso.png", rotation_speed=6)
-
 # STAGE = MatImage(MEDIA_BASE_DIR + "/mark-0.png")
-#_STAGE_FILES = [str(f) for f in Path(MEDIA_BASE_DIR).glob("mark*.png")]
-#STAGE = MatImageList(_STAGE_FILES)
-
-STAGE = MatSpin(file="/data/mark-bagua.png", rotation_speed=-1/5)
+_STAGE_FILES = [str(f) for f in Path(MEDIA_BASE_DIR).glob("marks/*.png")]
+STAGE = MatImageList(_STAGE_FILES)
 
 ###############################################################################
 # Stuff that really shouldn't change
@@ -55,17 +53,17 @@ STAGE = MatSpin(file="/data/mark-bagua.png", rotation_speed=-1/5)
 # 1 Screen at <pyglet.canvas.xlib.XlibDisplay object at 0x7fd600f9e750> at 2560 600 with 1920x1080
 # 2 Screen at <pyglet.canvas.xlib.XlibDisplay object at 0x7fd600f9e750> at 2560 0 with 1024x600
 
-DASH_SCREEN = 2
-PLAY_SCREEN = 1 
+DASH_SCREEN = 0
+PLAY_SCREEN = 1
 
 PLAY_FULLSCREEN = True
-DASH_FULLSCREEN = True
+DASH_FULLSCREEN = False
 
-CAMERA = 0
+CAMERA = 1
 CAMERA_SLEEP = 0.5
-SND_buzz = StaticSource(media_load(MEDIA_BASE_DIR + "/buzz.wav"))
-SND_shutter_start = StaticSource(media_load(MEDIA_BASE_DIR + "/shutter_start.wav"))
-SND_shutter_end = StaticSource(media_load(MEDIA_BASE_DIR + "/shutter_end.wav"))
+SND_buzz = StaticSource(media_load("img/buzz.wav"))
+SND_shutter_start = StaticSource(media_load("img/shutter_start.wav"))
+SND_shutter_end = StaticSource(media_load("img/shutter_end.wav"))
 
 if DEBUG:
     DASH_SCREEN = 0
