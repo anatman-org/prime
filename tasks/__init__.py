e4651922fdfe19684a017be5a15404cb79354f27
Thornton Prime <thornton.prime@gmail.com>
2023-11-15 13:14:02 -0800

    more task clean-up

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -49,6 +49,25 @@ def work_backup(ctx):
         {BACKUP_DESTINATION}"""
     )
 
+@task
+def frames2_vid(ctx, framebase):
+    from slugify import slugify
+
+    OUTPUT = slugify(framebase) + '.mp4'
+
+    # https://superuser.com/a/891478
+    FFMPEG = f"ffmpeg -framerate 6 -pattern_type glob -i '{framebase}' -vf 'scale=(iw*sar)*min(1920/(iw*sar)\\,1280/ih):ih*min(1920/(iw*sar)\\,1280/ih), pad=1920:1280:(1920-iw*min(1920/iw\\,1280/ih))/2:(1280-ih*min(1920/iw\\,1280/ih))/2' /work/clips/{OUTPUT}"
+    print(FFMPEG)
+    ctx.run(FFMPEG)
+
+@task 
+def vid2_frames(ctx, vid):
+    from slugify import slugify
+    OUTPUT = '/work/frames/' + slugify(vid) + '-%08d.png'
+    FFMPEG = f"ffmpeg -i {vid} -vf 'scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2' '{OUTPUT}'"
+    print(FFMPEG)
+    ctx.run(FFMPEG)
+
 
 @task
 def bits_update(ctx, bitbase):
@@ -59,7 +78,7 @@ def bits_update(ctx, bitbase):
         import reflink.error
     except ModuleNotFoundError:
         print(
-            "NO REFLINK, TRY export PYTHONPATH=/home/thornton/.local/lib/python3.11/site-packages"
+            "NO REFLINK, TRY export PYTHONPATH=$HOME/.local/lib/python3.12/site-packages"
         )
         return
         from shutil import copy2 as file_copy
@@ -89,7 +108,7 @@ def bits_update(ctx, bitbase):
                     xxh_hex = xxh.hexdigest()
 
                     xxh_path = (
-                        (bitdir / "bits") / xxh_hex[:2] / xxh_hex[2:4] / xxh_hex[4:]
+                        (bitdir / "bits") / xxh_hex[:2] / xxh_hex[2:4] / (xxh_hex[4:] + file.suffix)
                     )
 
                     if xxh_path.exists():
