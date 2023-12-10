cc99fa87343ded5022fb4aaab163b78159c7da03
Thornton Prime <thornton.prime@gmail.com>
2023-12-10 10:54:37 -0800

    lynn i ching

--- y/repage.py
+++ y/repage.py
@@ -8,14 +8,20 @@ from fitz import Document
 
 infile = "../read/I Ching (Lynn) [4ca0694e].pdf"
 outfile = "4ca0694e.pdf"
+tocfile = 'index.csv'
+startpage = 16
 
-startpage = 18
+pagelabels = [
+  {'startpage': 0, 'prefix': '', 'style': '', 'firstpagenum': 1},
+  {'startpage': 6, 'prefix': '', 'style': 'r', 'firstpagenum': 1},
+  {'startpage': startpage, 'prefix': '', 'style': 'D', 'firstpagenum': 1}
+]
 
-with Document(fname) as pdf:
+toc = [ (int(x[0]), x[1].strip().strip('"'), int(x[2]) + startpage)  for x in csv_reader(open(tocfile)) ]
 
-  csvfile = [ (int(x[0]), x[1].strip().strip('"'), int(x[2]) + startpage)  for x in csv_reader(open('siu.csv')) ]
+with Document(infile) as pdf:
+  pdf.set_page_labels( pagelabels )
+  pdf.set_toc(toc)
+  pdf.save(outfile, garbage=4, clean=True, deflate=True)
 
-  pdf.set_toc(csvfile)
-  pdf.save(fnew, garbage=4, clean=True, deflate=True)
 
-  
